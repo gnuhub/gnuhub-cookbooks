1bd852a4a77cf8f82f16ea7ff6481deae84b759e
Hiro Asari <asari.ruby@gmail.com>
2014-08-03 10:43:56 +0200

    Add haveged cookbook

---
+++ ci_environment/haveged/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Travis CI Development Team"
+license          "Apache v2.0"
+name             "haveged"
+version          "1.0.0"
+description      "Entropy source using HAVEGE algorithm"
+long_description "Entropy source using HAVEGE algorithm"
+
+recipe "haveged", "Installs haveged"
